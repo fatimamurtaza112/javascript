90c969a0e0c648b682f030d74069ed4138a5297f
fatima murtaza <fatimamurtaza5522@gmail.com>
2023-08-26 06:27:10 -0700

    template

---
+++ arrow.js
@@ -0,0 +1,24 @@
+// ES5
+function welcome(a, b) {
+
+    let result =a+b;
+console.log(a)
+console.log(b)
+    return result;
+
+}
+
+
+
+welcome =(a,b)=> a+b;
+
+
+// ES6
+let welcome = (a, b) => {
+
+    let result =a+b;
+    console.log(a)
+    console.log(b)
+        return result;
+};
+
---
+++ template.js
@@ -0,0 +1,9 @@
+var name = "Fatima";
+var age = 21;
+console.log("hello " + name + " " + age +" ")
+
+//template litrals
+console.log(`hello ${name} ${age}`)
+
+
+
