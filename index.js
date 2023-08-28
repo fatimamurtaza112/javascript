e125457d8eb0232ee8f16bd343b00c6f6e774434
fatima murtaza <fatimamurtaza5522@gmail.com>
2023-08-28 08:29:51 -0700

    import export

---
+++ index.js
@@ -0,0 +1,17 @@
+import { add } from './maths.js';
+import addition from './maths.js';
+
+let sum = add(3, 4)
+console.log(sum)
+sum = addition(4, 5)
+console.log(sum)
+
+//import export == named and default
+
+
+
+
+// linking file
+// same file
+// one level up
+// one level deep
---
+++ maths.js
@@ -0,0 +1,7 @@
+export const add = (a, b) => {
+    return a + b;
+}
+
+export default function sum(a, b) {
+    return a + b;
+}
