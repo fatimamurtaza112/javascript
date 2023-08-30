687fd7d434af39f2ed4690940b9694a1365964d6
fatima murtaza <fatimamurtaza5522@gmail.com>
2023-08-30 04:14:51 -0700

    first commit

--- arrow.js
+++ arrow.js
@@ -1,23 +1,41 @@
 // ES5
-function welcome(a, b) {
+//function welcome(a, b) {
 
-    let result =a+b;
-console.log(a)
-console.log(b)
-    return result;
+   // let result =a+b;
+//console.log(a)
+//console.log(b)
+   // return result;
 
-}  
+//}  
 
 
 
 // ES6 if returning a single line
-welcome =(a,b)=> a+b;
-// ES6 for returning multiple lines
-let welcome = (a, b) => {
+//welcome =(a,b)=> a+b;
+//es6 if returning multiple value
+//let welcome=(a,b)=>{
+    //let result=a+b;
+    //console.log(a)
+    //console.log(b)
+//}
+//task 2
+ //function practise
 
-    let result =a+b;
+
+function hello (a,b){
+   let result=a+b;
     console.log(a)
     console.log(b)
-        return result;
-};
+}
 
+//es6 for returinig one value
+
+welcome =(a,b)=> a+b;
+
+
+//es6 for returining multiple values
+let welcome=(a,b)=>{
+    let result=a+b;
+    console.log(a)
+    console.log(b)
+}
